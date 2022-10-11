025f6cf037125987ed4a16f0ceec5dc5c9383b25
EmmanuelFlorencioT <emm.ftr2@gmail.com>
2022-10-11 14:23:44 -0500

    First commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThreadSums
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
